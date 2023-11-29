2dd0e856b1c8fb439f50ee74863e90960013cd81
jakemoss127 <jacobmoss@ufl.edu>
2023-11-29 17:01:48 -0500

    Initial commit, front page prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
+import githubLogo from './assets/github-logo.png'
 
 function App() {
+  const appRef = useRef(null);
+  
+  useEffect( () => {
+
+    const moveGradient = (event) => {
+      const windowWidth = window.innerWidth; 
+      const windowHeight = window.innerHeight; 
+
+      const mouseX = Math.round((event.pageX / windowWidth) * 100)
+      const mouseY = Math.round((event.pageY / windowHeight) * 100)
+
+      if(appRef) {
+        appRef.current.style.setProperty("--mouse-x", mouseX.toString() + "%")
+        appRef.current.style.setProperty("--mouse-y", mouseY.toString() + "%")
+      }
+    };
+
+    document.addEventListener("mousemove", moveGradient);
+    return function cleanup() {
+      document.removeEventListener("mousemove", moveGradient);
+    };
+  }, [appRef])
+
+  const toGithub = () => {
+    const myUrl = 'https://github.com/jakemoss127'; 
+    window.open(myUrl, '_blank');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app" id="app" ref={appRef}>
+      <div className='content-container'>
+        <span className='title'>MOSS</span>
+        <p className='description'>
+          HELLO YOU!<br />
+          I'M <span className='name-title'>JACOB MOSS</span> AND THIS IS MY PORTFOLIO.<br />
+          YES, I CODE AND HAVE A <span className='passion-title'>PASSION</span> FOR CREATIVITY.<br/>
+          AND YES, I ALSO LOVE GRADIENTS.<br />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img className='git-logo' src={githubLogo} alt='github-logo' onClick={toGithub}/>
+      </div>
     </div>
   );
 }
