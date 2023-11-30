05e3ae68cd607e50b02cf161f81e8c580b025902
jakemoss127 <jacobmoss@ufl.edu>
2023-11-30 18:13:28 -0500

    Skills update, added GitHub copilot as well.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
 
   const projects = [
     {
-      title: "React.js",
+      title: "Java",
       color: "#F06318"
     },
     {
@@ -38,7 +38,7 @@ const Projects = () => {
       color: "#0CBCF0"
     },
     {
-      title: "Java",
+      title: "React",
       color: "#91F018"
     }
   
@@ -51,15 +51,48 @@ const Projects = () => {
     gsap.to(e.target, {top: "0", backgroundColor: "white", duration: 0.3, delay: 0.1})
   }
 
+  const navigateSite = (e, project) => {
+    switch(project.title) {
+      case "React":
+        window.open('https://react.dev/', '_blank');
+        break;
+      case "C++":
+        window.open('https://cplusplus.com/', '_blank');
+        break;
+      case "Java":
+        window.open('https://www.java.com/en/', '_blank');
+        break;
+      case "HTML5, CSS, JS":
+        window.open('https://www.freecodecamp.org/news/html-css-and-javascript-explained-for-beginners/', '_blank');
+        break;
+      case "Microsoft SQL Server":
+        window.open('https://www.microsoft.com/en-us/sql-server', '_blank');
+        break;
+      case "Figma":
+        window.open('https://www.figma.com/community', '_blank');
+        break;
+      case "UI/UX Design":
+        window.open('https://www.figma.com/community', '_blank');
+        break;
+      case "GitHub":
+        window.open('https://github.com', '_blank');
+        break; 
+      default:
+        break;
+    }
+  }
+
   return (
     <div className='project-wrapper'>
       <main>
+        <h1 className='my-skills'>My Skills</h1>
         {
           projects.map((project, index) => {
             return (
               <div
                 onMouseEnter={(e) => { manageMouseEnter(e, index) }}
                 onMouseLeave={(e) => { manageMouseLeave(e, index) }}
+                onClick={(e) => {navigateSite(e, project)}}
                 key={index}
               >
                 <p>{project.title}</p>
